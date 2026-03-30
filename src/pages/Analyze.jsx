29b81bf7a1265acdcf8a5b92757ed47165b11c06
yonna25 <yonnadigit@gmail.com>
2026-03-30 10:15:54 +0100

    Update Analyze.jsx

--- src/pages/Analyze.jsx
+++ src/pages/Analyze.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"; 
+import React, { useState, useEffect } from 'react'
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import { analyzeFace } from "../services/faceAnalysis.js";
