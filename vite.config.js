ab8a0643ea760389acdcd9301007a429fabf4e7f
yonna25 <yonnadigit@gmail.com>
2026-03-31 23:47:58 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  envPrefix: ['VITE_', 'FEDAPAY_PUBLIC'],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icons/*.png', 'icons/*.svg'],
+      manifest: {
+        name: 'AfroTresse',
+        short_name: 'AfroTresse',
+        description: 'Trouve ta tresse parfaite grâce à un selfie',
+        theme_color: '#2C1A0E',
+        background_color: '#2C1A0E',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+            handler: 'CacheFirst',
+            options: { cacheName: 'google-fonts', expiration: { maxAgeSeconds: 60 * 60 * 24 * 365 } }
+          }
+        ]
+      }
+    })
+  ],
+  server: {
+    proxy: {
+      '/api': { target: 'http://localhost:3001', changeOrigin: true }
+    }
+  }
+})
