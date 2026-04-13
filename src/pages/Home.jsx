9ed44d27e5ffab830003bbd75e61674fb5998397
yonna25 <yonnadigit@gmail.com>
2026-04-13 17:19:13 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -109,7 +109,7 @@ export default function Home() {
         </div>
 
         {/* TEXTE REMONTÉ */}
-        <div className="absolute inset-x-0 bottom-0 z-30 px-5 pb-52">
+        <div className="absolute inset-x-0 bottom-0 z-30 px-5 pb-32">
           <h1 className="font-display text-2xl text-white font-bold leading-tight">
             Une selfie, et découvre<br />
             Ta meilleure coiffure
