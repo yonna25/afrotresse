7b080b689e0de223813beeeb71dd9aa72fe8de07
yonna25 <yonnadigit@gmail.com>
2026-04-23 10:29:00 +0100

    Fix typo in SUPABASE_URL environment variable

--- api/credits.js
+++ api/credits.js
@@ -5,7 +5,7 @@
 import { createClient } from '@supabase/supabase-js'; // ou votre ORM
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
+  process.env.SUPABASEE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
