8cdc802fcf51c4815c09b4fc16103e7d1b3ac216
yonna25 <yonnadigit@gmail.com>
2026-05-04 00:33:53 +0100

    Add Vercel analytics injection in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { HelmetProvider } from 'react-helmet-async'
 // On ajoute l'extension .jsx pour aider Vercel à résoudre le fichier
 import App from './App.jsx' 
 import './index.css'
+import { inject } from '@vercel/analytics'
+inject()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
