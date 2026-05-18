17dd6d27013c90789e5872a72ddfdb011fd50a20
yonna25 <yonnadigit@gmail.com>
2026-05-18 23:44:05 +0100

    Add initial prop to AnimatePresence component

--- src/App.jsx
+++ src/App.jsx
@@ -165,7 +165,7 @@ function AnimatedRoutes() {
 
   return (
     <>
-      <AnimatePresence mode="wait">
+      <AnimatePresence mode="wait" initial={false}>
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Home />} />
           <Route path="/camera" element={<Camera />} />
@@ -277,5 +277,5 @@ export default function App() {
       </div>
     </BrowserRouter>
   )
-            }
+      }
     
